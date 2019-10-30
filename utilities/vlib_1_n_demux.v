f078837261b0acaad4fc2b18acb8f563b912a969
Andrew Tran <andrew.tran@axonny.com>
2019-10-29 17:59:11 -0700

    updated vlib_1_n_demux.v to support whether invalid output is zeroout

--- utilities/vlib_1_n_demux.v
+++ utilities/vlib_1_n_demux.v
@@ -35,6 +35,7 @@
 module vlib_1_n_demux
     #(parameter OUT_WORD_CNT = 32,
       parameter WORD_SZ = 8,
+      parameter INVALID_OUT_DATA_TO_ZERO = 1,   // 0: mirror in_data to all; 1: make invalid data become zeros
       parameter IDX_SZ = $clog2(OUT_WORD_CNT)
     )
     (
@@ -45,12 +46,33 @@ module vlib_1_n_demux
     output logic [OUT_WORD_CNT-1:0]                 out_vldmask
     );
     
-    logic [IDX_SZ-1:0] [2**(IDX_SZ-1)-1:0] [WORD_SZ-1:0]   lvl_data;
+    //==================== BODY ===================
+`ifdef FPGA    
+    always @* begin
+        out_vldmask = '0;
+        out_vldmask[in_output_idx] = 1'b1;
+    end
+generate
+  if (INVALID_OUT_DATA_TO_ZERO==1) begin
+    always @* begin
+        out_data = '0;
+        out_data[in_output_idx] = in_data;
+    end
+  end
+  else begin
+    always @* begin
+      for (int jj=0; jj<OUT_WORD_CNT; jj++) begin
+        out_data[jj] = in_data;
+      end
+    end    
+  end
+endgenerate
+    
+`else    
+generate
+//     logic [IDX_SZ-1:0] [2**(IDX_SZ-1)-1:0] [WORD_SZ-1:0]   lvl_data;
     logic [IDX_SZ-1:0] [2**(IDX_SZ-1)-1:0]                 lvl_vld;
 
-    
-    //==================== BODY ===================
-generate
 if (OUT_WORD_CNT == 1) begin: out_cnt_1
     assign out_vldmask[0] = 1'b1;
     assign out_data[0] = in_data;
@@ -58,41 +80,62 @@ end
 
 else if (OUT_WORD_CNT == 2) begin: out_cnt_2
     assign out_vldmask[0] = ~in_output_idx[0] ? 1'b1 : 1'b0;
-    assign out_data[0] = out_vldmask[0] ? in_data : {WORD_SZ{1'b0}};
     assign out_vldmask[1] = in_output_idx[0] ? 1'b1 : 1'b0;
-    assign out_data[1] = out_vldmask[1] ? in_data : {WORD_SZ{1'b0}};
     
+  if (INVALID_OUT_DATA_TO_ZERO==1) begin
+    assign out_data[0] = out_vldmask[0] ? in_data : {WORD_SZ{1'b0}};
+    assign out_data[1] = out_vldmask[1] ? in_data : {WORD_SZ{1'b0}};
+  end
+  else begin
+    assign out_data[0] = in_data;
+    assign out_data[1] = in_data;
+  end
 end
 
 else begin: out_cnt_greater_2
-    assign lvl_data[0][0] = in_data;
+//     assign lvl_data[0][0] = in_data;
     assign lvl_vld[0][0] = 1'b1;
 
-        for (genvar ii=1; ii<IDX_SZ; ii++) begin: lvl_ii
-            for (genvar jj=0; jj<2**ii; jj++) begin: col_jj
-                if (jj%2) begin: jj_even
-                    assign lvl_vld[ii][jj] = in_output_idx[IDX_SZ-ii] ? lvl_vld[ii-1][jj/2] : 1'b0;
-                end
-                else begin: jj_odd
-                    assign lvl_vld[ii][jj] = ~in_output_idx[IDX_SZ-ii] ? lvl_vld[ii-1][jj/2] : 1'b0;
-                end
-
-                assign lvl_data[ii][jj] = lvl_data[ii-1][jj/2];
-            end
-        end
-
-        for (genvar jj=0; jj<OUT_WORD_CNT; jj++) begin: last_lvl_col_jj 
-            if (jj%2) begin: jj_even 
-                assign out_vldmask[jj] = in_output_idx[0] ? lvl_vld[IDX_SZ-1][jj/2] : 1'b0;
+    for (genvar ii=1; ii<IDX_SZ; ii++) begin: lvl_ii
+        for (genvar jj=0; jj<2**ii; jj++) begin: col_jj
+            if (jj%2) begin: jj_even
+                assign lvl_vld[ii][jj] = in_output_idx[IDX_SZ-ii] ? lvl_vld[ii-1][jj/2] : 1'b0;
             end
             else begin: jj_odd
-                assign out_vldmask[jj] = ~in_output_idx[0] ? lvl_vld[IDX_SZ-1][jj/2] : 1'b0;
+                assign lvl_vld[ii][jj] = ~in_output_idx[IDX_SZ-ii] ? lvl_vld[ii-1][jj/2] : 1'b0;
             end
 
-            assign out_data[jj] = out_vldmask[jj] ? lvl_data[IDX_SZ-1][jj/2] : {WORD_SZ{1'b0}};
+//             assign lvl_data[ii][jj] = lvl_data[ii-1][jj/2];
         end
+    end
+
+    for (genvar jj=0; jj<OUT_WORD_CNT; jj++) begin: last_lvl_col_jj 
+        if (jj%2) begin: jj_even 
+            assign out_vldmask[jj] = in_output_idx[0] ? lvl_vld[IDX_SZ-1][jj/2] : 1'b0;
+        end
+        else begin: jj_odd
+            assign out_vldmask[jj] = ~in_output_idx[0] ? lvl_vld[IDX_SZ-1][jj/2] : 1'b0;
+        end
+
+//         assign out_data[jj] = out_vldmask[jj] ? lvl_data[IDX_SZ-1][jj/2] : {WORD_SZ{1'b0}};
+    end
+  if (INVALID_OUT_DATA_TO_ZERO==1) begin
+    always @* begin
+      for (int jj=0; jj<OUT_WORD_CNT; jj++) begin
+        out_data[jj] = out_vldmask[jj] ? in_data : {WORD_SZ{1'b0}};
+      end
+    end
+  end
+  else begin
+    always @* begin
+      for (int jj=0; jj<OUT_WORD_CNT; jj++) begin
+        out_data[jj] = in_data;
+      end
+    end
+  end
 end
 endgenerate
+`endif
 
 endmodule
 `endif // __VLIB_1_N_DEMUX_V__
